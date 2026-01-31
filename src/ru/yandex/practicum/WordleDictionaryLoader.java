fea16706e4fab122540f1fb23be9791cfa8a9649
Saharniu_Chelovek <Ssannya1177@bk.ru>
2026-01-31 03:05:38 +0300

    повторное исправление оформления

--- src/ru/yandex/practicum/WordleDictionaryLoader.java
+++ src/ru/yandex/practicum/WordleDictionaryLoader.java
@@ -6,7 +6,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.PriorityQueue;
+
 
 /*
 этот класс содержит в себе всю рутину по работе с файлами словарей и с кодировками
